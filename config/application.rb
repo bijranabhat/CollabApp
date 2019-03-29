f9a8e6d4fb03822f302d2179681f508552a69330
Bijay Ranabhat <bijayranabhat_2019@depauw.edu>
2019-03-29 23:21:26 +0000

    Configure and create helpers -Change include_all_helpers config to False -Split the _collapsible_elements.html.erb file's content into   partials and extract logic from the file into partials

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,13 @@ module CollabApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    #****added later so that Rails doesn't load all helpers files to all views
+    #Method names for different helper files might clash.
+    config.action_controller.include_all_helpers = false
+    #Now helpers are available for corresponding controller’s views only.
+    # So if we have the PagesController, all helpers inside the pages_helper.rb
+    # file will be available to all view files inside the pages directory.
+    
   end
 end
