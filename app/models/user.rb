8f8679120979e56802be76033c3fefc6bfb54f6b
Bijay Ranabhat <bijayranabhat_2019@depauw.edu>
2019-04-04 22:02:39 +0000

    -Generated Post and Category Model -Created associations between User, Post, and Category models -created categories and post database tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts, dependent: :destroy
+  # The dependent: :destroy argument says, when a user gets deleted,
+  # all posts what the user has created will be deleted too.
 end
