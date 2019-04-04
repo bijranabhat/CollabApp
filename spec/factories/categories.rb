08a256e3c63e11cd217c60aec5061c3ca4fc53ac
Bijay Ranabhat <bijayranabhat_2019@depauw.edu>
2019-04-04 22:24:52 +0000

    Added post and categories factories

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
+  #Created a category factory inside the factories directory
   factory :category do
-    
+    sequence(:name) { |n| "name#{n}" }
+    sequence(:branch) { |n| "branch#{n}" }
   end
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :post do
-    
+    #created a post factory
+    #easy to set up associtaion 
+    title 'a' * 20
+    content 'a'* 20
+    user
+    category
   end
 end
